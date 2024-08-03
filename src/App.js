7ed8fcfd274a230723a1c60f9502f86b99854b9a
Morganiron <86537433+Morganiron@users.noreply.github.com>
2024-08-02 20:39:14 -0500

    Basic boilerplate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutMe from './components/AboutMe';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
 import Contact from './components/Contact';
+import Header from './components/Header';
+import Project from './components/Project';
+import Skills from './components/Skills';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Header />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Contact />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about-me" component={AboutMe} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Project} />
+          <Route path="/skills" component={Skills} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
-      {/* Add your component content here */}
-    </div>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about-me">About Me</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
   );
 };
 
