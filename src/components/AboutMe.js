304262b917bbef6db9f82d8b4e02c36ec5954192
Morganiron <86537433+Morganiron@users.noreply.github.com>
2024-08-02 22:17:30 -0500

    Update to About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,13 +5,7 @@ const AboutMe = () => {
     <div>
       <h2>About Me</h2>
       <p>
-        I recently completed my Associate of Science in Software and Database Development and am now pursuing a Bachelor of Science in Software Engineering at Western Governors University, with a focus on C#. I have substantial experience with Java, having developed an Android app for my A.S. capstone course. This app utilized APIs from YouTube, Google, Firebase, and GitHub to create a user-driven, mood-based song suggestion platform aimed at providing better song recommendations based on real user feedback. The goal was to aid people in processing their emotions through music therapy.
-      </p>
-      <p>
-        My curiosity spans a broad range of subjects, including history, geology, astronomical sciences, art, psychology, technology, and mechanical engineering. I enjoy learning, sharing knowledge, and analyzing information to create a comprehensive understanding of various topics. Additionally, I've explored machine learning using Python, developing simple programs for analyzing stock and crypto prices, identifying patterns, and making predictions.
-      </p>
-      <p>
-        I am passionate about finding a role that combines my diverse skills and interests to make a positive impact and help others.
+        As a software engineer with a passion for solving complex problems, I blend technical expertise with a love for learning. My journey from developing a mood-based song suggestion app to exploring machine learning reflects my commitment to using technology to enhance lives. Outside of coding, I delve into history, art, and the sciences, continually seeking to connect diverse interests and create impactful solutions.
       </p>
     </div>
   );
