24ca89ce943e12acd91c9b58f33d1e31a4595ca1
Morganiron <86537433+Morganiron@users.noreply.github.com>
2024-08-02 20:40:07 -0500

    Added AboutMe content

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,8 +3,16 @@ import React from 'react';
 const AboutMe = () => {
   return (
     <div>
-      <h2>AboutMe</h2>
-      {/* Add your component content here */}
+      <h2>About Me</h2>
+      <p>
+        I recently completed my Associate of Science in Software and Database Development and am now pursuing a Bachelor of Science in Software Engineering at Western Governors University, with a focus on C#. I have substantial experience with Java, having developed an Android app for my A.S. capstone course. This app utilized APIs from YouTube, Google, Firebase, and GitHub to create a user-driven, mood-based song suggestion platform aimed at providing better song recommendations based on real user feedback. The goal was to aid people in processing their emotions through music therapy.
+      </p>
+      <p>
+        My curiosity spans a broad range of subjects, including history, geology, astronomical sciences, art, psychology, technology, and mechanical engineering. I enjoy learning, sharing knowledge, and analyzing information to create a comprehensive understanding of various topics. Additionally, I've explored machine learning using Python, developing simple programs for analyzing stock and crypto prices, identifying patterns, and making predictions.
+      </p>
+      <p>
+        I am passionate about finding a role that combines my diverse skills and interests to make a positive impact and help others.
+      </p>
     </div>
   );
 };
