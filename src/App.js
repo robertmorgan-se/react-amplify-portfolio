b4b7f2f547703f726e3bcff6662951e9aab6b9d3
Morganiron <86537433+Morganiron@users.noreply.github.com>
2024-08-02 20:53:09 -0500

    Fixed typo in Project to Projects

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
 import Header from './components/Header';
-import Project from './components/Project';
+import Project from './components/Projects';
 import Skills from './components/Skills';
 import Home from './pages/Home';
 
@@ -16,7 +16,7 @@ const App = () => {
           <Route path="/" exact component={Home} />
           <Route path="/about-me" component={AboutMe} />
           <Route path="/contact" component={Contact} />
-          <Route path="/projects" component={Project} />
+          <Route path="/projects" component={Projects} />
           <Route path="/skills" component={Skills} />
         </Switch>
       </div>
