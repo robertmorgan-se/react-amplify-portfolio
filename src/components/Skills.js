b76be0d4d2088646aeb7049eb1da225d5700961f
Morganiron <86537433+Morganiron@users.noreply.github.com>
2024-08-04 18:32:32 -0500

    Added content and styles for Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,32 @@
+/* src/components/Skills.js */
+
 import React from 'react';
 import './Skills.css';
 
 const Skills = () => {
   return (
     <div className="skills-container">
-      <p className="skills-text">
-        Test
-      </p>
+      <section className="skills-section">
+        <h3>Front End</h3>
+        <p className="skills-text">HTML5, CSS3, JavaScript, React</p>
+      </section>
+      <section className="skills-section">
+        <h3>Back End</h3>
+        <p className="skills-text">SQL, Firebase, AWS Amplify</p>
+      </section>
+      <section className="skills-section">
+        <h3>Programming Languages</h3>
+        <p className="skills-text">Python, Java, C#, C++, JavaScript</p>
+      </section>
+      <section className="skills-section">
+        <h3>Tools & Technologies</h3>
+        <p className="skills-text">GitHub, Visual Studio, Firebase, Oracle SQL Developer, MySQL</p>
+      </section>
+      <section className="skills-section">
+        <h3>Other Relevant Skills</h3>
+        <p className="skills-text">User Interface(UI), Android Development, Analysis Skills, Research, IoT</p>
+      </section>
+      
     </div>
   );
 };
