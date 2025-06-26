9f7826588c1d963c5d56c6d638780b509bde709a
Morganiron <rsmorgan1985@gmail.com>
2025-06-26 13:49:34 -0500

    feat(routing): add hidden FamilyUpload page- wrap App in BrowserRouter in index.js- define /family route in App.js- create FamilyUpload component and CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from './components/layout/Header';
 import Home from './pages/Home';
+import FamilyUpload from './pages/FamilyUpload';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="bg-slate-100 min-h-screen text-gray-800 antialiased">
       <Header />
       <main className="mx-auto px-4 py-6">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/family-upload" element={<FamilyUpload />} />
+        </Routes>
       </main>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './output.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +9,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
